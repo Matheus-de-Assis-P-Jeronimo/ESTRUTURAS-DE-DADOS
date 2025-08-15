8512c3692db898a5f167a71c782fb4fae3697ff1
Matheus Assis P. Jerônimo <matheusassis9696@gmail.com>
2025-08-14 22:12:40 -0300

    Add files via upload

---
+++ Aula-1.py
@@ -0,0 +1,12 @@
+var = input ("Qual seu Objetivo? ")
+var1 = "Hello"
+var2 = "World"
+var3 = "!!!!!!"
+print ("Então seu objetivo é", var, "?")
+print (type(var3))#Ver o Tipo da variavel
+numeroInteiro = int(input ("Digite um número "))
+numeroVirgula = float(input("Digite outro número, não se acanhe "))
+if(numeroVirgula >= 1000):
+    print("Vamo com calma calabreso, Estava falando de número fracionado ")
+elif(numeroVirgula <= 0.000001):
+    print("Não é pra tanto tambem Arrombado")
