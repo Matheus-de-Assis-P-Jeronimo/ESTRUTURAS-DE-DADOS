7724885bce906e58e8391f7305755c394517a081
Matheus Assis P. Jerônimo <matheusassis9696@gmail.com>
2025-08-21 22:23:17 -0300

    fim da aula 2

--- Aula 2 21_08/Aula-2.py
+++ Aula 2 21_08/Aula-2.py
@@ -1,12 +1,30 @@
-
-AnoAtual = int(input("Qual o ano atual? "))
-AnoNasc = int(input("Qual o ano de nascimento? "))
-ResultadoAno = AnoAtual - AnoNasc
-if ResultadoAno <=9:
-    print("Você é uma criança.")
-elif ResultadoAno > 9 and ResultadoAno <= 19:
-    print("Você é um adolescente.")
-elif ResultadoAno > 19 and ResultadoAno <= 60:
-    print("Você é um adulto.")
-elif ResultadoAno > 60:
-    print("Você é um idoso.") 
+# Exercicio 1
+# AnoAtual = int(input("Qual o ano atual? "))
+# AnoNasc = int(input("Qual o ano de nascimento? "))
+# ResultadoAno = AnoAtual - AnoNasc
+# if ResultadoAno <=9:
+#     print("Você é uma criança.")
+# elif ResultadoAno > 9 and ResultadoAno <= 19:
+#     print("Você é um adolescente.")
+# elif ResultadoAno > 19 and ResultadoAno <= 60:
+#     print("Você é um adulto.")
+# elif ResultadoAno > 60:
+#     print("Você é um idoso.") 
+#elif ResultadoAno < 0:
+#     print("Fala aê o Poeira cósmica.") 
+#---------------------------------------------------
+#Exercicio 2 
+# x = 1
+# while x <= 3:
+#     print(x)
+#     x += 1
+#---------------------------------------------------
+#Exercicio 3
+n = 100
+soma = 0
+i = 1
+while (i<= n):
+    soma += i
+    i += 1
+    print("Soma de 1 a até {n}: {soma}")
+    break #força a parada da estrutura de repetição
